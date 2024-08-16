6ad9c452bf1771e84c9407d3ff7eb8ae16f0cb0b
lethanh-dev <lethanh.1701.dev@gmail.com>
2024-08-16 19:03:38 +0700

    add

--- BE/BK.js
+++ BE/BK.js
@@ -7,7 +7,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '7430422460:AAHWBefQH7clQPt8AbCfZuuqlpJ2fsP7Tt8';
 const CHAT_ID = '-1002162225326';
 const MONGO_URI = 'mongodb://localhost:27017/crypto-bot'; // URI kết nối MongoDB
-const BACKUP_PATH = 'root/CryptoBot/BE/bk/'; // Đường dẫn lưu trữ sao lưu
+const BACKUP_PATH = 'bk/'; // Đường dẫn lưu trữ sao lưu
 const BACKUP_FILE = path.join(BACKUP_PATH, `backup.gz`); // Tên file sao lưu
 
 const bot = new TelegramBot(TOKEN);
@@ -40,7 +40,7 @@ function backupDatabase() {
 // Hàm gửi file sao lưu đến Telegram
 function sendBackupToTelegram() {
     return new Promise((resolve, reject) => {
-        bot.sendDocument(CHAT_ID, BACKUP_FILE, {
+        bot.sendDocument(CHAT_ID, "backup.gz", {
             caption: `Backup | ${new Date().toLocaleString()}`
         })
             .then(() => {
