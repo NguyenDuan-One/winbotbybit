59af952eeb3b4929e4ef4f274229724a0e34118c
lethanh-dev <lethanh.1701.dev@gmail.com>
2024-08-17 15:51:24 +0700

    add

--- BE/runTrade-dev.js
+++ BE/runTrade-dev.js
@@ -1199,10 +1199,6 @@ const handleSocketBotApiList = async (botApiListInput = {}) => {
                                             const side = dataMain.side
                                             const openTrade = +dataMain.entryPrice  //Gia khop lenh
 
-                                            const size = Math.abs(dataMain.size)
-
-                                            missTPDataBySymbol[botSymbolMissID].sizeTotal = size
-
                                             const missSize = size - missTPDataBySymbol[botSymbolMissID].size
 
                                             const Quantity = side === "Buy" ? size : (size * -1)
